296631f67bbef2f421356a598c20af42924352f6
WiLynd <hdqphu.dev@gmail.com>
2021-12-01 16:10:29 +0700

    01/12

--- src/ss1_introduction_java/bai_tap/MoneyExchange.java
+++ src/ss1_introduction_java/bai_tap/MoneyExchange.java
@@ -1,4 +1,16 @@
 package ss1_introduction_java.bai_tap;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class MoneyExchange {
+    public static void main(String[] args) {
+        double vnd = 23000;
+        double usd;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập số tiền USD: ");
+        usd = scanner.nextDouble();
+        double exchange = usd*vnd;
+        System.out.println("Quy đổi: "+ exchange +" vnđ");
+    }
 }
