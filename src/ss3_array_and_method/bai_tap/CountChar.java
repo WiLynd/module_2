bf9c9d38b91959bb9799408987afe7b48b8084cd
WiLynd <hdqphu.dev@gmail.com>
2021-12-02 14:53:29 +0700

    02/12

---
+++ src/ss3_array_and_method/bai_tap/CountChar.java
@@ -0,0 +1,21 @@
+package ss3_array_and_method.bai_tap;
+
+import java.util.Scanner;
+
+public class CountChar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String str;
+        char ch;
+        int count = 0;
+        System.out.print("Input string: ");
+        str = input.nextLine();
+        System.out.print("Input character: ");
+        ch = input.next().charAt(0);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == ch)
+                count++;
+        }
+        System.out.println("Count: " + count);
+    }
+}
